76b1473ba38c0104e05343b198a84188c84d818d
Hubert Théau <worldof123789@hotmail.com>
2025-04-29 22:46:36 +0200

    Initial commit

---
+++ src/client/java/irs/modid/InteriorRainSoundClient.java
@@ -0,0 +1,10 @@
+package irs.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InteriorRainSoundClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
